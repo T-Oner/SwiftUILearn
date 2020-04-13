c1957b16bbeb43336d1171f63cc0fe0eb54edc21
T-Oner <1@fxcdev.com>
2020-04-13 09:11:45 +0800

    Add PokeMaster

---
+++ PokeMaster/PokeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeMaster
+//
+//  Created by 冯旭超 on 2020/4/12.
+//  Copyright © 2020 冯旭超. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
